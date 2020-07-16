aa0c7c4acaeb9a22ce87621c136eb01cfb75fc7b
MA20011300 <MA20011300@APL-31R4KW2>
2020-07-16 16:42:06 +0530

    Cleanup

--- AzureServiceBus/src/main/java/com/azureservicebus/learnings/sender/MessageSender.java
+++ AzureServiceBus/src/main/java/com/azureservicebus/learnings/sender/MessageSender.java
@@ -7,14 +7,15 @@ import org.springframework.core.env.Environment;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
-/*JmsTemplate simplifies receiving and sending of messages through JMS and gets rid of the boilerplate code 
- *It is the helper class that simplifies synchronous JMS access code and central class for Spring JMS integration
+/*
+ * JmsTemplate simplifies receiving and sending of messages through JMS and gets rid of the boilerplate code 
+ * It is the helper class that simplifies synchronous JMS access code and central class for Spring JMS integration
  *In JmsTemplate by default the transaction and acknowledgement mode are disable, 
- *if you want transaction and acknowledgement we need to configure on property file.
- *While doing transaction we need to take care settings. 
+ * if you want transaction and acknowledgement we need to configure on property file.
+ * While doing transaction we need to take care settings. 
  *
- *convertAndSend method take the destination name and message. 
- *it will dynamically take the destination resource name and bind to JMS destination resolver 
+ * convertAndSend method take the destination name and message. 
+ * it will dynamically take the destination resource name and bind to JMS destination resolver 
  */
 
 @Component
@@ -31,11 +32,9 @@ public class MessageSender {
 	public void postMessage(String megString) {
 		logger.debug("Start : postMessage() ");
 		String queueName = env.getProperty("azure.servicebus.queuename");
-
 		logger.info("Message is {}", megString);
 		jmsTemplate.convertAndSend(queueName, megString);
 		logger.info("Message has been sent :)  ");
-
 		logger.debug("End : postMessage() ");
 	}
 
